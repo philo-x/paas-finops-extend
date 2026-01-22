f3e75c18453c428e82c6d42752a6cbea0b13f86a
胥寅 <xuuyin@dingtalk.com>
2026-01-22 16:06:41 +0800

    1、新增生成Dockerfile 2、发送mq之前，记录MQ内容

--- config/mq.go
+++ config/mq.go
@@ -1,8 +1,9 @@
 package config
 
 type MQ struct {
-	Url     string `mapstructure:"url" json:"url" yaml:"url"`
-	Topic   string `mapstructure:"topic" json:"topic" yaml:"topic"`
-	Tag     string `mapstructure:"tag" json:"tag" yaml:"tag"`
-	Timeout int    `mapstructure:"timeout" json:"timeout" yaml:"timeout"`
+	Url      string `mapstructure:"url" json:"url" yaml:"url"`
+	Topic    string `mapstructure:"topic" json:"topic" yaml:"topic"`
+	Tag      string `mapstructure:"tag" json:"tag" yaml:"tag"`
+	Timeout  int    `mapstructure:"timeout" json:"timeout" yaml:"timeout"`
+	Receiver string `mapstructure:"receiver" json:"receiver" yaml:"receiver"`
 }
--- model/observe/mq_message.go
+++ model/observe/mq_message.go
@@ -9,18 +9,20 @@ type MQMessageRequest struct {
 
 // MQAlertData MQ告警数据
 type MQAlertData struct {
-	EmailSubject string        `json:"邮件主题"`
-	AlertDetail  MQAlertDetail `json:"告警详情"`
+	Title       string        `json:"title"`    // 原 邮件主题
+	Receiver    string        `json:"receiver"` // 新增
+	AlertDetail MQAlertDetail `json:"detail"`   // 原 告警详情
 }
 
 // MQAlertDetail 告警详情
 type MQAlertDetail struct {
-	AlertStatus  string `json:"告警状态"`
-	AlertLevel   string `json:"告警等级"`
-	AlertCluster string `json:"告警集群"`
-	AlertObject  string `json:"告警对象"`
-	PolicyName   string `json:"策略名称"`
-	AlertDesc    string `json:"告警描述"`
-	TriggerValue string `json:"触发数值"`
-	AlertTime    string `json:"告警时间"`
+	Status       string `json:"status"`       // 原 告警状态
+	Severity     string `json:"severity"`     // 原 告警等级
+	Cluster      string `json:"cluster"`      // 原 告警集群
+	Object       string `json:"object"`       // 原 告警对象
+	Indicator    string `json:"indicator"`    // 原 策略名称
+	Summary      string `json:"summary"`      // 原 告警描述
+	TriggerValue string `json:"triggerValue"` // 原 触发数值
+	AlertTime    string `json:"alertTime"`    // 原 告警时间
+	Remark       string `json:"remark"`       // 新增
 }
